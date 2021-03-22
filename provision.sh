413932513dccc4c959f99f6f6405d8345fb8caba
Alexandre Montesinos Cavallé <58102601+traxito@users.noreply.github.com>
2021-03-22 21:22:00 +0100

    Docker add

--- provision.sh
+++ provision.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
-sudo yum install epel-release bash-completion tree wget curl sftp python-argcomplete -y
+#install utils and Docker
+sudo yum install epel-release bash-completion tree wget curl sftp python-argcomplete yum-utils -y
+ sudo yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
 systemctl stop firewalld
 systemctl disable firewalld
-#install containerd
-cat <<EOF | sudo tee /etc/modules-load.d/containerd.conf
-overlay
-br_netfilter
-EOF
-sudo modprobe overlay
-sudo modprobe br_netfilter
-sudo mkdir -p /etc/containerd
-containerd config default | sudo tee /etc/containerd/config.toml
-sudo systemctl restart containerd
+sudo yum install docker-ce docker-ce-cli containerd.io
+sudo systemctl start docker
+#install containerd (prepared for when the installation is automated detected for Kubeadm)
+#cat <<EOF | sudo tee /etc/modules-load.d/containerd.conf
+#overlay
+#br_netfilter
+#EOF
+#sudo modprobe overlay
+#sudo modprobe br_netfilter
+#sudo mkdir -p /etc/containerd
+#containerd config default | sudo tee /etc/containerd/config.toml
+#sudo systemctl restart containerd
 #Using the systemd cgroup driver
-sed -i 's/^SystemdCgroup = false.*/SystemdCgroup = true/g' /etc/containerd/config.toml
-sudo systemctl restart containerd
+#sed -i 's/^SystemdCgroup = false.*/SystemdCgroup = true/g' /etc/containerd/config.toml
+#sudo systemctl restart containerd
 # Setup required sysctl params, these persist across reboots.
 cat <<EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
 net.bridge.bridge-nf-call-iptables  = 1
